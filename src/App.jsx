06ec96b57d9f055815b4efcf9a3a63d2cfac4c22
nurudeen <access2abey@gmail.com>
2023-05-28 23:17:53 +0100

    creating the login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+
+import { LoginPage} from './pages';
+//import { Header } from './components';
 
 
 function App() {
 
   return (
     <div>
+      <Routes>
+            <Route path="/" element={<LoginPage />} >
+                
+                {/* <Route path="/signup" element={<SignUpPage />} /> */}
+                
+
+                
+                {/* <Route path="*" element={<NotFoundPage />} /> */}
+            </Route>
+            
+        </Routes>
       <h1>
         App Component
       </h1>
