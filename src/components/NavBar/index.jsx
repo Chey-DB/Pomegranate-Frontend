465fa0bbb9912daba0d4a5b9e7b166a99818ff3a
mithurssan <126930607+mithurssan@users.noreply.github.com>
2023-05-31 10:55:25 +0100

    add css to navbar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,16 +1,27 @@
 import React from "react"
 import { NavLink, Outlet } from "react-router-dom"
+import "./style.css"
 
 const NavBar = () => {
+    const activeStyle = {
+        textDecoration: "underline",
+        color: "#FFF6C3"
+    }
+
+    const navA = ({ isActive }) => (isActive ? activeStyle : undefined)
+
     return (
         <>
             <header>
                 <nav>
-                    <NavLink to="/">Home</NavLink>
-                    <NavLink to="/leaderboard">Leaderboard</NavLink>
-                    <NavLink to="/profile">Profile</NavLink>
-                    <NavLink to="/login">Login</NavLink>
-                    <NavLink to="/signup">Signup</NavLink>
+                    <h2>Pomegranate</h2>
+                    <ul className="nav-links">
+                        <li><NavLink style={navA} to="/">Home</NavLink></li>
+                        <li><NavLink style={navA} to="/leaderboard">Leaderboard</NavLink></li>
+                        <li><NavLink style={navA} to="/profile">Profile</NavLink></li>
+                        <li><NavLink style={navA} to="/login">Login</NavLink></li>
+                        <li><NavLink style={navA} to="/signup">Signup</NavLink></li>
+                    </ul>
                 </nav>
             </header>
             <Outlet />
