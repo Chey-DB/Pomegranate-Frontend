1e7707fbf6f1ff9b766f1049e83fcddac946ce8f
mithurssan <126930607+mithurssan@users.noreply.github.com>
2023-06-01 15:04:36 +0100

    update render links and fix menu

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router'
 
 import { Menu, Tasks, Timer } from '../../components'
 
 const Profile = () => {
+    const { username } = useParams();
     return (
         <div>
-            <Menu />
+            <Menu username={username} />
             <Tasks />
             <Timer />
         </div>
