cb52d7dfe7fbd2cf6f3468592374409a08435c6c
ABF22 <bofalade@gmail.com>
2023-06-02 09:09:44 +0100

    added test

---
+++ src/pages/Login/_test_/Login.test.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import fetchMock from 'fetch-mock';
+import App from './index';
+
+describe('App component', () => {
+  beforeEach(() => {
+    fetchMock.mock('*', { tasks: [] });
+  });
+
+  afterEach(() => {
+    fetchMock.restore();
+  });
+
+  it('fetches data and renders the tasks correctly', async () => {
+    render(<App />);
+
+    
+    await screen.findByText('Task 1');
+
+    // Perform your assertions here
+    const task1 = screen.getByText('Task 1');
+    expect(task1).toBeInTheDocument();
+    // for this test you must npm install --save-dev fetch-mock dont ask why
+
+  });
+});
